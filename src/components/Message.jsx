9d128c4b006dd5a71a0ab2b49689cd6cd9e66c63
aminopyridin <aminopyrodin@gmail.com>
2018-02-14 13:38:41 +0500

    del constructor

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,10 +3,6 @@ import {format} from "../emoji.js";
 import styles from "./Message.css";
 
 export default class Message extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className={ styles.message } >
