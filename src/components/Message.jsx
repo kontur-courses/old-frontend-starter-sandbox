19bd6368aceb9ef6c12b5a867cbcf59c241f1899
aminopyridin <aminopyrodin@gmail.com>
2018-02-14 13:34:31 +0500

    del highlight, update react packages
    # Conflicts:
    #	package.json

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,28 +5,11 @@ import styles from "./Message.css";
 export default class Message extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { highlighted: false };
-    }
-
-    highlight() {
-        this.setState({ highlighted: true });
-    }
-
-    unhighlight() {
-        this.setState({ highlighted: false });
     }
 
     render() {
-        var classes = styles.message;
-        if (this.state.highlighted) {
-            classes += " " + styles.highlighted
-        }
         return (
-            <div
-                className={classes}
-                onMouseOver={() => this.highlight()}
-                onMouseLeave={() => this.unhighlight()}
-            >
+            <div className={ styles.message } >
                 {this.props.user}: {format(this.props.text)}
             </div>
         );
